d465417e70574af341a185162cbe308fe31e1c84
MartínTrujillo Bustamante <martin.trujillo1105@gmail.com>
2019-09-07 08:19:55 -0500

    sdw

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-	<meta charset="iso-8859-1">
-	<meta name="description" content="La Capilla Santa Rosa tuvo sus origenes en la época de los yanaconas del Ex Fundo Santa Rosa...">
+	<meta http-equiv="Content-Type" content="text/html"; charset="utf-8"/> 
+	<meta name="description" content="La Capilla Santa Rosa tuvo sus origenes en la Ã©poca de los yanaconas del Ex Fundo Santa Rosa...">
 	<meta name="keywords" content="capilla,iglesia,parroquia,santa,rosa">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<meta name="theme-color" content="#f8d000">
 	<meta name="twitter:card" content="summary">
-	<meta name="twitter:title" content="Comunidad Santa Rosa | Parroquia Nuestra Señora de las Misiones">
+	<meta name="twitter:title" content="Comunidad Santa Rosa | Parroquia Nuestra SeÃ±ora de las Misiones">
 	<meta name="twitter:image" content="http://www.comunidadsantarosa.org/images/logo.jpg">
 	<meta name="twitter:description" content="La Capilla Santa Rosa tuvo sus orígenes en la época de los yanaconas del Ex Fundo Santa Rosa. Fue construida en el actual terreno el 30 de Agosto de 1955...">
 	<title>Comunidad Santa Rosa</title>
@@ -26,16 +26,7 @@
         <!-- Post Content Column -->
         <div class="col-lg-8">
           <h4 class="mt-4">XXXIII DOMINGO DEL TIEMPO ORDINARIO</h4>
-          <h4 class="mt-1">Amar a Dios con todo el corazon, con toda el alma y con todo el Ser</h4>		  
-
-          <!-- Author -->
-          <p class="lead">
-            Por
-            <a href="#">Mart&iacute;n Trujillo</a>          </p>
-
-          <!--hr>
-
-          <!-- Date/Time -->
+          <h4 class="mt-1">Amar a Dios con todo el corazón, con toda el alma y con todo el Ser</h4>		  
           <p>Publicado el 06 Setiembre, 2019</p>
 
           <!--hr>
@@ -46,34 +37,34 @@
           <hr>
 
           <!-- Post Content -->
-          <p class="lead">"Seguir a Jesus no significa participar en un cortejo triunfal. Significa entrar en su gran obra de misericordia, de perdon, de amor. Y este perdon universal, esta misericordia, pasa a través de la cruz".</p>
+          <p class="lead">"Seguir a Jesús no significa participar en un cortejo triunfal. Significa entrar en su gran obra de misericordia, de perdón, de amor. Y este perdón universal, esta misericordia, pasa a través de la cruz".</p>
 		  
           <blockquote class="blockquote">
             <p class="mb-0"><b>1era Lectura</b></p>
-            <footer class="blockquote-footer">Lectura del libro de la Sabiduria
+            <footer class="blockquote-footer">Lectura del libro de la Sabiduría
               <cite title="Source Title">9,13-18</cite>            </footer>
           </blockquote>
 
-          <p>¿Que hombre conoce los proyectos de Dios? ¿Quien comprende lo que Dios quiere?. Los pensamientos de los mortales son frágiles, e inseguras nuestras reflexiones; porque el cuerpo mortal es un peso para el alma, y esta morada terrena oprime a la mente que medita. Apenas conocemos las cosas terrenas y con trabajo encontramos lo que esta a nuestro alcance. Pues,, ¿quien rastreará las cosas del cielo? ¿Quien conocera tu designio, si tu no le das sabiduria, enviando tu santo espiritu desde el cielo?. Solo asi se enderezaron los caminos de quienes habitan la tierra, los hombres aprendieron lo que te agrada, y la sabiduria los salvo.<br>
+          <p>¿Que hombre conoce los proyectos de Dios? ¿Quien comprende lo que Dios quiere?. Los pensamientos de los mortales son frágiles, e inseguras nuestras reflexiones; porque el cuerpo mortal es un peso para el alma, y esta morada terrena oprime a la mente que medita. Apenas conocemos las cosas terrenas y con trabajo encontramos lo que esta a nuestro alcance. Pues, ¿quién rastreará las cosas del cielo? ¿Quién conocera tu designio, si tu no le das sabiduría, enviando tu santo espíritu desde el cielo?. Solo así se enderezaron los caminos de quienes habitan la tierra, los hombres aprendieron lo que te agrada, y la sabiduría los salvo.<br>
             Palabra de Dios. R. Te alabamos Señor.</p>
 
           <blockquote class="blockquote">
             <p class="mb-0"><b>Salmo(89)</b></p>
-            <footer class="blockquote-footer">Señor, tu haz sido nuestro refudio de generacion en generacion.            </footer>
+            <footer class="blockquote-footer">Señor, tu haz sido nuestro refudio de generación en generación.            </footer>
           </blockquote>
 
-          <p>- Tu reduces al hombre a polvo, diciendo: &quot;Retornen, hijos de Adan&quot;. Mil años en tu presencia son un ayer, que paso; una vela nocturna.<br>
+          <p>- Tu reduces al hombre a polvo, diciendo: &quot;Retornen, hijos de Adan&quot;. Mil años en tu presencia son un ayer, que pasó; una vela nocturna.<br>
             - Los siembras año por año, como hierba que se renueva: que florece y se renueva por la mañana, y por la tarde se marchita y seca.<br>
-            - Enseñanos a calcular nuestros años, para que adquiramos un corazón sensato. Vuévete Señor, ¿hasta cuando?. Ten compasion de tus siervos.<br>
-            - Por la mañana sacianos de tu misericordia, y toda nuestra vida sera alegria y jubilo. Baje a nosotros la bondad del Señor y haga prosperas las obras de nuestras manos.</p>
+            - Enseñanos a calcular nuestros años, para que adquiramos un corazón sensato. Vuelvete Señor, ¿hasta cuando?. Ten compasión de tus siervos.<br>
+            - Por la mañana sacianos de tu misericordia, y toda nuestra vida sera alegría y júbilo. Baje a nosotros la bondad del Señor y haga prosperas las obras de nuestras manos.</p>
 
           <blockquote class="blockquote">
             <p class="mb-0"><b>2da Lectura</b></p>
-            <footer class="blockquote-footer">Lectura de la cara del apostol san Pablo a Filemon
+            <footer class="blockquote-footer">Lectura de la carta del apóstol san Pablo a Filemón
               <cite title="Source Title">9b-10.12-17</cite>            </footer>
           </blockquote>
 
-          <p>Querido hermano: Yo, Pablo, ya anciano y ahora tambien prisionero por Cristo Jesus, te recomiendo a Onesimo, mi hijo, a quien he engendrado en la prision; te lo envio como si te enviara mi propio corazon. Me hubiera gustado retenerlo juanto a mi, para que me sirviera en tu lugar, en esta prision que sufro por el Evangelio; pero no he querido retenerlo sin contar contigo; asi me haras este favor, no a la fuerza, sino con libertad. Quiza el te abandono por breve  tiempo, precisamente para que ahora lo recuperes para siempre; y no como esclavo, sino mucho mejor: como hermano muy querido. Si yo lo quiero tanto, cuanto mas lo has de querer tu, como hombre y como cristiano. Si me consideras compañero tuyo, recibelo a el como si me recibieras a mi.<br>
+          <p>Querido hermano: Yo, Pablo, ya anciano y ahora también prisionero por Cristo Jesus, te recomiendo a Onésimo, mi hijo, a quién he engendrado en la prisión; te lo envío como si te enviara mi propio corazón. Me hubiera gustado retenerlo juanto a mí, para que me sirviera en tu lugar, en esta prisión que sufro por el Evangelio; pero no he querido retenerlo sin contar contigo; así me harás este favor, no a la fuerza, sino con libertad. Quizá el te abandono por breve  tiempo, precisamente para que ahora lo recuperes para siempre; y no como esclavo, sino mucho mejor: como hermano muy querido. Si yo lo quiero tanto, cuanto mas lo has de querer tu, como hombre y como cristiano. Si me consideras compañero tuyo, recíbelo a el como si me recibieras a mí.<br>
             Palabra de Dios. R. Te alabamos, Señor.</p>
 
           <blockquote class="blockquote">
@@ -83,54 +74,11 @@
           </blockquote>
 
 <p>R. Gloria a ti, Señor.<br>
-  En aquel tiempo, mucha gente acompañaba a Jesus; el se volvio y les dijo: Si alguno viene a mi y no me ama mas que a su padre y a su madre, y a su mujer y a sus hijos, y a sus hermanos y a sus hermanas, e incluso a si mismo, no puede ser discipulo mio. Quien no lleve su cruz detras de mi no puede ser discipulo mio. Asi, ¿quien de ustedes, si quiere construir una torre, no se sienta primero a calcular los gastos, a ver si tiene para terminarla?. No sea que, una vez puestos los cimientos, no puede acabarla y se pongan a burlarse de el los que miran, diciendo: &quot;Este hombre empezo a construir y no ha sido capaz de terminar&quot;. ¿O que rey, si va dar batalla a otro rey, no se sienta primero a deliberar si con diez mil hombres podran salir al paso del que le ataca con veinte mil? .<br>
-  Y si no puede, cuando el otro esta todavia lejos, envia delegados para pedir condiciones de paz. Lo mismo ustedes: el que no renuncia a todos sus bienes no puede ser discipulo mio.<br>
-  Palabra del Señor. R. Gloria a ti, Señor Jesus</p>
+  En aquel tiempo, mucha gente acompañaba a Jesús; el se volvió y les dijo: Si alguno viene a mi y no me ama mas que a su padre y a su madre, y a su mujer y a sus hijos, y a sus hermanos y a sus hermanas, e incluso a sí mismo, no puede ser discípulo mio. Quien no lleve su cruz detrás de mí no puede ser discípulo mío. Asi, ¿quién de ustedes, si quiere construir una torre, no se sienta primero a calcular los gastos, a ver si tiene para terminarla?. No sea que, una vez puestos los cimientos, no puede acabarla y se pongan a burlarse de el los que miran, diciendo: &quot;Este hombre empezó a construir y no ha sido capaz de terminar&quot;. ¿O que rey, si va dar batalla a otro rey, no se sienta primero a deliberar si con diez mil hombres podrán salir al paso del que le ataca con veinte mil? .<br>
+  Y si no puede, cuando el otro esta todavía lejos, envia delegados para pedir condiciones de paz. Lo mismo ustedes: el que no renuncia a todos sus bienes no puede ser discípulo mío.<br>
+  Palabra del Señor. R. Gloria a ti, Señor Jesús</p>
 
           <hr>
-
-          <!-- Comments Form -->
-		  <!--
-          <div class="card my-4">
-            <h5 class="card-header">Leave a Comment:</h5>
-            <div class="card-body">
-              <form>
-				<div class="form-group">
-				  Nombre:
-                  <input type="text" class="form-control" name="nombre">
-                </div>
-                <div class="form-group">
-				  Comentario:
-                  <textarea class="form-control" rows="3"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
-            </div>
-          </div>
-
-          <!-- Single Comment -->
-		  <!--
-          <div class="media mb-4">
-            <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-            <div class="media-body">
-              <h5 class="mt-0">Commenter Name</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </div>
-          </div>
-
-          <!-- Comment with nested comments -->
-		  <!--
-          <div class="media mb-4">
-            <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-            <div class="media-body">
-              <h5 class="mt-0">Commenter Name</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-
-
-
-            </div>
-          </div>
-			-->
         </div>
 
         <!-- Sidebar Widgets Column -->
@@ -138,7 +86,7 @@
 
      <!-- Recent post -->
           <div class="card my-4">
-            <h5 class="card-header">Articulos recientes </h5>
+            <h5 class="card-header">Artículos recientes </h5>
             <div class="card-body">
               <ul class="list-unstyle mb-0">
                 <li><a href="index.php">XXIII Domingo de tiempo ordinario </a></li>                              
@@ -163,9 +111,9 @@
 
           <!-- Side Widget -->
           <div class="card my-4">
-            <h5 class="card-header">Acerca de mi</h5>
+            <h5 class="card-header">Acerca de mí</h5>
             <div class="card-body">
-              La Capilla Santa Rosa tuvo sus origenes en la época de los yanaconas del Ex Fundo Santa Rosa...            </div>
+              La Capilla Santa Rosa tuvo sus orígenes en la época de los yanaconas del Ex Fundo Santa Rosa...            </div>
           </div>
         </div>
 
@@ -175,13 +123,6 @@
     </div>
     <!-- /.container -->
 
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Derechos reservados 2019</p>
-      </div>
-      <!-- /.container -->
-    </footer>
 
     <!-- Bootstrap core JavaScript -->
 	
